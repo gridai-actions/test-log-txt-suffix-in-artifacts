9c55b842f1a0b853aba237632a9846a8211a9c09
Robert S Lee <sangkyulee@gmail.com>
2021-10-29 07:39:26 -0400

    add 2nd test

--- run2.py
+++ run2.py
@@ -1,18 +1,20 @@
 from argparse import ArgumentParser
 from time import sleep
 
-def main(extension, sleep_time, close_file=false):
+def main(extension, sleep_time, close_file=0):
     with open(f"results.{extension}", "w") as outf:
         outf.write(f"Extension: {extension}\n")
         outf.write(f"Sleep: {sleep_time}\n")
     sleep(sleep_time)
-    if close_file:
-       close(outf)
+    if close_file != 0:
+       outf.close()
+    else:
+      print(f"not closing file on results.{extension} sleep {sleep_time}")   
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--extension", type=str, default="txt")
     parser.add_argument("--sleep", type=int, default=0)
-    parser.add_argument("--close", type=boolean, default=false)
+    parser.add_argument("--close_file", type=int, default=0)
     args = parser.parse_args()
-    main(args.extension, args.sleep)
+    main(args.extension, args.sleep, args.close_file)
