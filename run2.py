8aebb5a2e4ccb774eee34b0e3634e6c81c0dc075
Robert S Lee <sangkyulee@gmail.com>
2021-12-14 14:37:48 -0500

    make filename an HPO

--- run2.py
+++ run2.py
@@ -1,23 +1,25 @@
 from argparse import ArgumentParser
 from time import sleep
 
-def main(extension, sleep_time=0, close_file=0, flush_file=0):
-    with open(f"results.{extension}", "w") as outf:
-        outf.write(f"Extension: {extension}\n")
-        outf.write(f"Sleep: {sleep_time}\n")
-    sleep(sleep_time)
+def main(filename, extension, sleep_time=0, close_file=0, flush_file=0):
+    outf = open(f"{filename}.{extension}", "w")
+    outf.write(f"{filename}.{extension}\n")
+    outf.write(f"Sleep: {sleep_time}\n")
+    outf.write(f"{flush_file}: {flush_file}\n")
+    outf.write(f"{close_file}: {close_file}\n")
+    if sleep_time != 0:
+      sleep(sleep_time)
     if flush_file != 0:
        outf.flush()
     if close_file != 0:
        outf.close()
-    else:
-      print(f"not closing file on results.{extension} sleep {sleep_time}")   
 
 if __name__ == "__main__":
     parser = ArgumentParser()
+    parser.add_argument("--filename", type=str, default="results")
     parser.add_argument("--extension", type=str, default="txt")
     parser.add_argument("--sleep", type=int, default=0)
     parser.add_argument("--close_file", type=int, default=0)
     parser.add_argument("--flush_file", type=int, default=0)
     args = parser.parse_args()
-    main(args.extension, sleep_time=args.sleep, close_file=args.close_file, flush_file=args.flush_file)
+    main(args.filename, args.extension, sleep_time=args.sleep, close_file=args.close_file, flush_file=args.flush_file)
